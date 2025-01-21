ace350631a3596065cfda71d8695c99403e78094
Rene <reneener.dev@gmail.com>
2025-01-21 16:57:19 -0500

    Se crea el entity y los enum para las tareas

--- src/tareas/entities/tarea.entity.ts
+++ src/tareas/entities/tarea.entity.ts
@@ -1,1 +1,46 @@
-export class Tarea {}
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+import { EstadoTarea } from 'src/common/enums/estado-tarea.enum';
+import { IsNumber, IsOptional } from 'class-validator';
+
+@Entity()
+export class Tarea {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    descripcion: string;
+
+    @Column()
+    estado: EstadoTarea;
+
+    @Column()
+    adjunto_url: string;
+
+    @IsOptional()
+    @Column()
+    tarea_realizada_url?: string;
+
+    @IsOptional()
+    @Column()
+    @IsNumber()
+    costo?: number;
+
+    @Column({type:"timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP"})    fecha_envio: Date;
+
+    @Column(
+        {
+            type: "date",
+        }
+    )
+    fecha_a_realizar: Date;
+
+    @Column(
+        {
+            type: "date",
+        }
+    )
+    fecha_realizada: Date;  
+}
