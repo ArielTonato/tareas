c534a17bc330ee432f5dc94d13730f54cba9a356
Rene <reneener.dev@gmail.com>
2025-01-20 20:18:08 -0500

    Se hace la configuracion al env y a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot(
+      {
+        type: 'mysql',
+        host: process.env.dbHost,
+        port: +process.env.dbPort, //Poner el puerto de su base de datos
+        username: process.env.dbUser,
+        password: process.env.dbPassword,
+        database: process.env.dbDatabase,//Nombrar su base de esta manera
+        autoLoadEntities: true,
+        synchronize: true,
+      }
+    )
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
