53757c2c79bff8b729bbe01c4ba91f13583c02a7
Rene <reneener.dev@gmail.com>
2025-01-21 12:43:30 -0500

    Se corrige el DTO

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,3 +1,37 @@
+import { IsEmail, IsEnum, IsOptional, IsPhoneNumber, Matches, MinLength } from "class-validator";
+import { Instituciones } from "src/common/instituciones.enum";
+import { Rol } from "src/common/roles.enum";
 
 
-export class CreateUsuarioDto {}
+export class CreateUsuarioDto {
+    @MinLength(3, { message: "El nombre de usuario debe tener al menos 3 caracteres" })
+    nombre_usuario: string;
+
+    @Matches(/^[a-zA-Z\s]+$/, { message: 'El  nombre no puede contener números' })
+    @MinLength(3, { message: "El  nombre debe tener al menos 3 caracteres" })
+    nombre: string;
+
+    @Matches(/^[a-zA-Z\s]+$/, { message: 'El  apellido no puede contener números' })
+    @MinLength(3, { message: "El  apellido debe tener al menos 3 caracteres" })
+    apellido: string;
+
+    @Matches(/^[0-9]+$/, { message: 'El número debe contener solo números' })
+    @IsPhoneNumber("EC", { message: "El número de teléfono debe ser válido para Ecuador" })
+    numero: string;
+
+    @Matches(/^[a-zA-Z\s]+$/, { message: 'La carrera no puede contener números' })
+    @MinLength(3, { message: "La carrera debe tener al menos 3 caracteres" })
+    carrera: string;
+    @IsEmail({}, { message: "El correo no es valido" })
+    correo: string;
+
+    @IsEnum(Instituciones)
+    institucion: Instituciones;
+
+    @MinLength(6, { message: "La contraseña debe tener al menos 6 caracteres" })
+    password: string;
+
+    @IsOptional()
+    @IsEnum(Rol)
+    rol?: Rol;
+}
