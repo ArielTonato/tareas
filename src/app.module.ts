f6b61cf2917840b4bbd0de471e1db6e74050d9c9
Rene <reneener.dev@gmail.com>
2025-01-20 20:29:41 -0500

    Se crea el recurso usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './usuario/usuario.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,7 +18,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         autoLoadEntities: true,
         synchronize: true,
       }
-    )
+    ),
+    UsuarioModule
   ],
   controllers: [],
   providers: [],
