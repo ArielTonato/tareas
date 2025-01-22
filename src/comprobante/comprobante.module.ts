a9dd0fb28f779d4e7b08e361014bdbb887355857
CriSant20 <csantamaria8643@uta.edu.ec>
2025-01-22 12:17:28 -0500

    Se crea el comprobante DTO

--- src/comprobante/comprobante.module.ts
+++ src/comprobante/comprobante.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ComprobanteService } from './comprobante.service';
 import { ComprobanteController } from './comprobante.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comprobante } from './entities/comprobante.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Comprobante]),
+  ],
   controllers: [ComprobanteController],
   providers: [ComprobanteService],
 })
--- src/comprobante/dto/create-comprobante.dto.ts
+++ src/comprobante/dto/create-comprobante.dto.ts
@@ -1,1 +1,21 @@
-export class CreateComprobanteDto {}
+import { IsString, IsNumber, IsEnum, IsOptional } from 'class-validator';
+import { EstadoComprobante } from 'src/common/enums/estado-comprobante.enum';
+
+export class CreateComprobanteDto {
+    @IsNumber()
+    id_tarea: number;
+
+    @IsString()
+    url_comprobante: string;
+
+    @IsEnum(EstadoComprobante)
+    @IsOptional()
+    estado_comprobante?: EstadoComprobante;
+
+    @IsString()
+    @IsOptional()
+    comentario?: string;
+
+    @IsOptional()
+    fecha_revision?: Date;
+}
