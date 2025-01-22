00716f24246e978e03ac08d3f7627c222be9fcf0
CriSant20 <csantamaria8643@uta.edu.ec>
2025-01-22 16:12:16 -0500

    Se crea el DTO para la encuesta

--- src/encuesta/dto/create-encuesta.dto.ts
+++ src/encuesta/dto/create-encuesta.dto.ts
@@ -1,1 +1,23 @@
-export class CreateEncuestaDto {}
+import { IsEnum, IsNumber, Min, Max } from 'class-validator';
+import { Atencion } from "../enums/atencion.enum";
+import { Costo } from "../enums/costo.enum";
+import { Calidad } from "../enums/calidad.enum";
+
+export class CreateEncuestaDto {
+    @IsNumber()
+    id_tarea: number;
+
+    @IsEnum(Atencion)
+    atencion: Atencion;
+
+    @IsEnum(Costo)
+    costo: Costo;
+
+    @IsEnum(Calidad)
+    calidad: Calidad;
+
+    @IsNumber()
+    @Min(0)
+    @Max(10)
+    nota: number;
+}
