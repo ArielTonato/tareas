0e8dbe6a7d1838dcc04c74a4f4f4959c87070734
CriSant20 <csantamaria8643@uta.edu.ec>
2025-01-21 21:25:54 -0500

    Se crea la entidad comentario

--- src/comentarios/entities/comentario.entity.ts
+++ src/comentarios/entities/comentario.entity.ts
@@ -1,1 +1,19 @@
-export class Comentario {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Comentario {
+    @PrimaryGeneratedColumn()
+    id_comentario: number;
+
+    @Column()
+    id_tarea: number;
+
+    @Column()
+    id_usuario: number;
+
+    @Column()
+    comentario: string;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" }) 
+    fecha_comentario: Date;
+}
